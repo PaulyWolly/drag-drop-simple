0fefc0a84d50c1c0778e7c23c01158444eee1864
Paul Welby <pwelby@gmail.com>
2023-07-19 22:21:55 -0700

    New project: drag-drop-simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'drag-drop-simple';
+  // title = 'drag-drop-simple';
+
+  items = ['Football', 'Tennis', 'Basketball', 'Rugby', 'Golf']
+
+  title = 'dropzone';
+
+  incomingGoods = ['Tomatoes', 'Carrots', 'Onions', 'Pepper']
+
+  availableGoods = ['Cucumber']
+
+  soldGoods = ['Orange', 'Apple', 'Banana']
+
+
+  // drop(event: CdkDragDrop<string[]>) {
+  //   moveItemInArray(
+  //     this.items,
+  //     event.previousIndex,
+  //     event.currentIndex);
+  // }
+
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  dropped(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+    event.container.data,
+    event.previousIndex,
+    event.currentIndex
+      );
+     } else {
+       transferArrayItem(
+    event.previousContainer.data,
+    event.container.data,
+    event.previousIndex,
+    event.currentIndex
+      );
+    }
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// drag-drop cdk
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
